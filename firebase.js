59465917c43a63925a70d6ca0242c0e9f494459c
Ajay Khajindar <ajay.watchout@gmail.com>
2026-06-02 08:17:54 +0530

    Competitions added

---
+++ firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD8Wlr90FHRVG193mwL_Dwj5hoUEiysT74",
+    authDomain: "icon-house-points-2fb0a.firebaseapp.com",
+    projectId: "icon-house-points-2fb0a",
+    storageBucket: "icon-house-points-2fb0a.firebasestorage.app",
+    messagingSenderId: "401872110272",
+    appId: "1:401872110272:web:6c364b39afa1ecafae2325"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
